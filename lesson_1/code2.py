7d96d0899b75a00643a032e96290b462855ebdb1
vsevolod026 <vsevolod026@gmail.com>
2025-06-13 18:24:25 +0300

    ref: оставил только документацию, остальное удалили

--- lesson_1/code2.py
+++
@@ -1,8 +0,0 @@
-friends = ['anton', 'seva', 'igor', 'serega']
-
-for friend in friends:
-    if friend == 'seva':
-        continue
-
-    if friend.startswith('s'):
-        print(f'{friend}, твое имя начинается также как у меня!')
--- lesson_1/tmp.py
+++
@@ -1,3 +0,0 @@
-dictionary = {'seva': 1, 'anton': 2}
-print(repr(dictionary))
-print(dictionary)
